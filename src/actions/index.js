e446596c0c0cb2bed5ddf4ba3b70faf5a2f641fa
Royal <thaparoyal17@gmail.com>
2021-06-10 15:51:58 +0545

    creating streams through action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import streams from "../apis/streams";
 import { SIGN_IN, SIGN_OUT } from "./types";
 
 export const signIn = (userId) => {
@@ -10,5 +11,11 @@ export const signIn = (userId) => {
 export const signOut = () => {
   return {
     type: SIGN_OUT,
+  };
+};
+
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    streams.post("/streams", formValues);
   };
 };
