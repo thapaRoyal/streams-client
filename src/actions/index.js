8ae946de5f2a46c786be0f01e934e48e0ced7e45
Royal <thaparoyal17@gmail.com>
2021-06-11 15:07:49 +0545

    dispatching action after stream creation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -16,6 +16,7 @@ export const signOut = () => {
 
 export const createStream = (formValues) => {
   return async (dispatch) => {
-    streams.post("/streams", formValues);
+    const response = await streams.post("/streams", formValues);
+    dispatch({ type: CREATE_STREAM, payload: response.data });
   };
 };
