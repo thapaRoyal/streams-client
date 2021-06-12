628558f2cc15c153b366483a12c3a40583182a11
Royal <thaparoyal17@gmail.com>
2021-06-12 14:21:56 +0545

    creating a browser history object

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
+import history from "../history";
 
 const App = () => {
   return (
     <div className="ui container">
-      <Router>
+      <Router history={history}>
         <>
           <Header />
           <Route path="/" exact component={StreamList} />
---
+++ src/history.js
@@ -0,0 +1,4 @@
+// import createHistory from "history/createBrowserHistory";
+import { createBrowserHistory } from "history";
+
+export default createBrowserHistory();
