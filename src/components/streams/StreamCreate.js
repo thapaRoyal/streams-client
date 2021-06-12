7a33f86e67ce8c46bd8f1870254a0075f0fa485b
Royal <thaparoyal17@gmail.com>
2021-06-12 15:17:15 +0545

    refactoring stream creation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,72 +1,21 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions/index";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-    // console.log(meta);
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-        {/* <div>{meta.error}</div> */}
-      </div>
-    );
-  };
-
   onSubmit = (formValues) => {
-    // event.preventDefault();
-    // console.log(formValues);
     this.props.createStream(formValues);
   };
 
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        <Field name="title" component={this.renderInput} label="Enter title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errors = {};
-  if (!formValues.title) {
-    // only runs if user did not entered a title
-    errors.title = "You must  enter a title ";
-  }
-  if (!formValues.description) {
-    // only runs if user did not entered a description
-    errors.description = "You must  enter a description ";
-  }
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: "streamCreate",
-  validate,
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
