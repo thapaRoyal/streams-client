379d75000a779b9abaa6ea33b6f1ad25bba44184
Royal <thaparoyal17@gmail.com>
2021-06-12 15:38:19 +0545

    edit form submission

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -9,7 +9,8 @@ class StreamEdit extends React.Component {
     this.props.fetchStream(this.props.match.params.id);
   }
   onSubmit = (formValues) => {
-    console.log(formValues);
+    // console.log(formValues);
+    this.props.editStream(this.props.match.params.id, formValues);
   };
 
   render() {
