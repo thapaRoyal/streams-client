020f7a1eee99a947e748dd19de250ea9b304fe3e
Royal <thaparoyal17@gmail.com>
2021-06-13 11:32:29 +0545

    creating  a portal for modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Modal from "../Modal";
 
 const StreamDelete = () => {
   return (
     <div>
-      <h1>Stream Delete</h1>
+      <Modal />
     </div>
   );
 };
