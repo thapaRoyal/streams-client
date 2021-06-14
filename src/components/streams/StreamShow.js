bb7ba3d781118ad59ee6ccccbfbe8c9d699166f6
Royal <thaparoyal17@gmail.com>
2021-06-14 18:05:34 +0545

    optional player building

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -12,7 +12,18 @@ class StreamShow extends React.Component {
   componentDidMount() {
     const { id } = this.props.match.params;
 
-    this.props.fetchStream(this.props.match.params.id);
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+
     this.player = flv.createPlayer({
       type: "flv",
       url: `http://localhost:8000/live/${id}.flv`,
